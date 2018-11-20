8e36676f3996457d14b61d49be5d9ef07e1faa03
Thomas Gilbert <thomas.gilbert@sky.uk>
2018-11-20 16:08:40 +0000

    Added boilerplate

---
+++ app/components/Form/ExerciseGroup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class CreatePage extends React.PureComponent {
+  render() {
+    return (
+      <div className="form-group">
+        <input value />
+      </div>
+    );
+  }
+}
---
+++ app/containers/CreatePage/index.js
@@ -0,0 +1,32 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ *
+ * NOTE: while this component should technically be a stateless functional
+ * component (SFC), hot reloading does not currently support SFCs. If hot
+ * reloading is not a necessity for you then you can refactor it and remove
+ * the linting exception.
+ */
+
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class CreatePage extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+        <form>
+          <div className="form-group">
+            <input value />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
